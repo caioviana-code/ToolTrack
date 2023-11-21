9b47e651443838a67d6e3d0255525c46445b2bff
caioviana-code <dacamv@outlook.com>
2023-11-20 21:53:06 -0300

    ferramentaController

--- routes/web.php
+++ routes/web.php
@@ -14,11 +14,13 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('ferramentas.index');
 });
 
 Route::get('/dashboard', function () {
-    return view('dashboard');
+    return view('templates.middleware')->with('titulo', "");
 })->middleware(['auth'])->name('dashboard');
+
+Route::resource('ferramentas', 'FerramentaController');
 
 require __DIR__.'/auth.php';
